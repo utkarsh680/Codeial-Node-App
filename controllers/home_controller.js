d5c710d7eea24f093d960c587e6151897384e289
Utkarsh Singh <singhrishu680@gmail.com>
2023-10-10 22:18:04 +0530

    Display Post and related User

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,8 +1,17 @@
-module.exports.home = function(req, res){
-   return res.render('home', {
-      title: "Home"
-   })
-}
+const Post = require("../models/post");
 
+module.exports.home = async function (req, res) {
+  try {
+    // Populate the "user" field to get associated user information
+    const posts = await Post.find({}).populate('user').exec();
+    return res.render("home", {
+      title: "Codeial | Home",
+      posts: posts
+    });
+  } catch (err) {
+    console.error("Error fetching posts:", err);
+    return res.status(500).send("Internal Server Error");
+  }
+};
 
 //module.exports.action = function(req, res){}
