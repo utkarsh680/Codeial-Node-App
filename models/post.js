be687079bbb2ea2272b1d3e284555d142a032377
Utkarsh Singh <singhrishu680@gmail.com>
2023-10-11 23:20:48 +0530

    Add Comment Schema

--- models/post.js
+++ models/post.js
@@ -1,18 +1,28 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const postSchema = new mongoose.Schema({
-  content: {
-    type: String,
-    require: true,
+const postSchema = new mongoose.Schema(
+  {
+    content: {
+      type: String,
+      require: true,
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    // include the array of ids of all comments in this post schema itself
+    comments: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Comment",
+      },
+    ],
   },
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User'
+  {
+    timestamps: true,
   }
-}, {
-    timestamps: true
-});
+);
 
-const Post = mongoose.model('Post', postSchema);
+const Post = mongoose.model("Post", postSchema);
 
 module.exports = Post;
